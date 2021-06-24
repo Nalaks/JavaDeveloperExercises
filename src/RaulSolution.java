69e6929d34957cdd8e1e7181d7d4894d0bf86be1
Stefan <insertinsight@gmail.com>
2021-06-24 16:27:26 +0200

    raul update

---
+++ src/RaulSolution.java
@@ -0,0 +1,17 @@
+public class RaulSolution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+
+            for (int j = 0; j < 11; j++) {
+                System.out.print("x ");
+            }
+            System.out.println();
+            for (int k = 0; k < 4; k++) {
+                System.out.println("X         X         X");
+            }
+        }
+        for (int j = 0; j < 11; j++) {
+            System.out.print("x ");
+        }
+    }
+}
