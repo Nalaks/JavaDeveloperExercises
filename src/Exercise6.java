452f02f3abe7a08c44867c098264b5ad86db2a14
Stefan <insertinsight@gmail.com>
2021-06-25 15:29:01 +0200

    exercise6 update

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -1,11 +1,16 @@
 public class Exercise6 {
     public static void main(String[] args) {
-        System.out.println("Mo  Di  Mi  Do  Fr  Sa  So");
-        System.out.println("             1   2   3   4");
-        System.out.println(" 5   6   7   8   9  10  11");
-        System.out.println("12  13  14  15  16  17  18");
-        System.out.println("19  20  21  22  23  24  25");
-        System.out.println("26  27  28  29  30  31    ");
+        String[] monat = new String[6];
 
+        monat[0] = "Mo  Di  Mi  Do  Fr  Sa  So";
+        monat[1] = "             1   2   3   4";
+        monat[2] = " 5   6   7   8   9  10  11";
+        monat[3] = "12  13  14  15  16  17  18";
+        monat[4] = "19  20  21  22  23  24  25";
+        monat[5] = "26  27  28  29  30  31    ";
+
+        for (int i = 0; i < monat.length; i++) {
+            System.out.println(monat[i]);
+        }
     }
 }
